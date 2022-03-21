614ee055b7c07744e5549ddea81f999d31c6529d
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-21 23:18:59 +0600

    data lode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import "./Shop.css";
+
+const Shop = () => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+      fetch('products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+    }, [])
+    
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        <h2>Products Container: {products.length}</h2>
+      </div>
+      <div className="cart-container">
+        <h3>Order summary</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
