cf4098d77959f8471e93846ee49d7c0bd53a69dd
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-23 14:16:02 +0600

    product quantity problem fixt

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,13 +2,12 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({cart}) => {
-console.log(cart);
     let total = 0;
     let shipping = 0;
     let quantity = 0;
     for (const product of cart) {
         quantity = quantity + product.quantity
-        total = total + product.price
+        total = total + product.price * product.quantity
         shipping = shipping + product.shipping
     }
     const tax = parseFloat((total * 0.1).toFixed(2));
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -15,10 +15,21 @@ const Shop = () => {
       .then(data => setProducts(data))
     }, [])
 
-    const handleAddToCart = (product) => {
-      const newCart = [...cart, product];
+    const handleAddToCart = (selectedProduct) => {
+      console.log(cart);
+      let newCart = [];
+      const exists = cart.find((product) => product.id === selectedProduct.id);
+      if (!exists) {
+        selectedProduct.quantity = 1
+        newCart = [...cart, selectedProduct];
+      }
+      else{
+        const rest = cart.filter((product) => product.id !== selectedProduct.id);
+        exists.quantity = exists.quantity + 1
+        newCart = [...rest, exists]
+      }
       setCart(newCart)
-      addToDb(product.id)
+      addToDb(selectedProduct.id)
     }
     
     useEffect(() => {
