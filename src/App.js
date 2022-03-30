35d76848574fe71365842c844edf25585499ba91
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-30 22:12:38 +0600

    Order , Inventory & About components add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Components/About/About";
 import Header from "./Components/Header/Header";
+import Inventory from "./Components/Inventory/Inventory";
+import Orders from "./Components/Orders/Orders";
 import Shop from "./Components/Shop/Shop";
 
 function App() {
@@ -10,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Shop></Shop>}></Route>
         <Route path="/shop" element={<Shop></Shop>}></Route>
+        <Route path="/orders" element={<Orders></Orders>}></Route>
+        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
       </Routes>
     </div>
   );
