e7ac52a9fbbfcceed40dc54dc9f0474c33ef2396
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-31 12:48:15 +0600

    delete btn click then remove item for db

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,6 +1,7 @@
 import React from "react";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
+import { removeFromDb } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import ReviewItem from "../ReviewItem/ReviewItem";
 
@@ -11,6 +12,7 @@ const Orders = () => {
   const handleRemoveProduct = (product) => {
     const rest = cart.filter((pd) => pd.id !== product.id);
     setCart(rest)
+    removeFromDb(product.id)
   };
   return (
     <div className="shop-container">
