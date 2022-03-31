b4a3d7c7912490fd1aca241fa1c6dbfe9761cb7a
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-31 07:21:22 +0600

    useCart added

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,11 +1,14 @@
 import React from "react";
+import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
 
 const Orders = () => {
   const [products] = useProducts();
+  const [cart, setCart] = useCart(products)
   return (
     <div>
       <h1>This is Orders: {products.length}</h1>
+      <h1>This is Orders: {cart.length}</h1>
     </div>
   );
 };
---
+++ src/hooks/useCart.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { getStoreCart } from "../utilities/fakedb";
+
+
+const useCart = (products) => {
+    const [cart, setCart] = useState([])
+    useEffect(() => {
+        const storeCart = getStoreCart();
+        const saveCart = [];
+        for (const id in storeCart) {
+            const addedProduct = products.find((product) => product.id === id);
+            if (addedProduct) {
+                 const quantity = storeCart[id];
+                 addedProduct.quantity = quantity
+                 saveCart.push(addedProduct)
+            }
+        }
+        setCart(saveCart)
+    }, [products])
+    
+    return [cart, setCart]
+};
+
+export default useCart;
