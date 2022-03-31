6b274ce50a2a85880a0c91adcd20524b13c22a43
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-31 09:59:16 +0600

    review cart style add & product details added

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,14 +1,22 @@
 import React from "react";
 import useCart from "../../hooks/useCart";
 import useProducts from "../../hooks/useProducts";
+import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Orders = () => {
   const [products] = useProducts();
-  const [cart, setCart] = useCart(products)
+  const [cart] = useCart(products);
   return (
-    <div>
-      <h1>This is Orders: {products.length}</h1>
-      <h1>This is Orders: {cart.length}</h1>
+    <div className="shop-container">
+      <div className="review-item-container">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart key={cart.id} cart={cart}></Cart>
+      </div>
     </div>
   );
 };
---
+++ src/Components/ReviewItem/ReviewItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./ReviewItem.css";
+
+const ReviewItem = ({ product }) => {
+  const { img, name, price, shipping, quantity } = product;
+  return (
+    <div className="review-item">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div className="review-item-details-container">
+          <div className="review-item-details">
+              <p className="product-name">{name}</p>
+              <p>Price: <span className="orange-color">{price}</span></p>
+              <p><small>Shipping: {shipping}</small></p>
+              <p><small>Quantity: {quantity}</small></p>
+          </div>
+          <div className="delete-container"><button>Delete</button></div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewItem;
