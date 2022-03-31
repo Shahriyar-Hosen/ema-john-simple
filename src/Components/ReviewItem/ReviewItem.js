68fa65e49c0f4135102ca92c528ca80de082973b
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-03-31 10:00:59 +0600

    ternary operator & title adde in name

--- src/Components/ReviewItem/ReviewItem.js
+++ src/Components/ReviewItem/ReviewItem.js
@@ -9,13 +9,23 @@ const ReviewItem = ({ product }) => {
         <img src={img} alt="" />
       </div>
       <div className="review-item-details-container">
-          <div className="review-item-details">
-              <p className="product-name">{name}</p>
-              <p>Price: <span className="orange-color">{price}</span></p>
-              <p><small>Shipping: {shipping}</small></p>
-              <p><small>Quantity: {quantity}</small></p>
-          </div>
-          <div className="delete-container"><button>Delete</button></div>
+        <div className="review-item-details">
+          <p className="product-name" title={name}>
+            {name.length > 20 ? name.slice(0, 20) + "..." : name}
+          </p>
+          <p>
+            Price: <span className="orange-color">{price}</span>
+          </p>
+          <p>
+            <small>Shipping: {shipping}</small>
+          </p>
+          <p>
+            <small>Quantity: {quantity}</small>
+          </p>
+        </div>
+        <div className="delete-container">
+          <button>Delete</button>
+        </div>
       </div>
     </div>
   );
