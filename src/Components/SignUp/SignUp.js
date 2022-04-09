83245a43a94629a9a35e0a26e36cecee8c0ca75e
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-10 02:02:04 +0600

    sign up section & style add

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-    return (
-        <div>
-            <h2>This is SignUp page</h2>
+  return (
+    <div className="form-container">
+      <div>
+        <h4 className="form-title">Sign Up</h4>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" password="email" id="" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input type="password" password="confirm-password" id="" />
+          </div>
+        </form>
+        <div className="form-submit">
+          <input type="submit" value="Sign Up" />
         </div>
-    );
+        <p>
+          Already have an account?
+          <Link className="form-link" to="/signup">
+            Login
+          </Link>
+        </p>
+        <div className="or-line">
+          <hr />
+          <span>Or</span>
+          <hr />
+        </div>
+        <div>
+          <button className="continue-google">
+            <img src="../../images/google.png" alt="" /> Continue with Google
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
