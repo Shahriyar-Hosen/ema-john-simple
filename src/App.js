b6cad613eb4bc9c7c12222706b373e872689bbd0
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-10 00:04:04 +0600

    login link & login, signup route add

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import "./App.css";
 import About from "./Components/About/About";
 import Header from "./Components/Header/Header";
 import Inventory from "./Components/Inventory/Inventory";
+import Login from "./Components/Login/Login";
 import Orders from "./Components/Orders/Orders";
 import Shop from "./Components/Shop/Shop";
+import SignUp from "./Components/SignUp/SignUp";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<Shop></Shop>}></Route>
-        <Route path="/shop" element={<Shop></Shop>}></Route>
-        <Route path="/orders" element={<Orders></Orders>}></Route>
-        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/" element={<Shop />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+        <Route path="/orders" element={<Orders />}></Route>
+        <Route path="/inventory" element={<Inventory />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
       </Routes>
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../images/Logo.svg';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../images/Logo.svg";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div><Link to="/shop">Shop</Link>
-            <Link to="/orders">Orders</Link>
-            <Link to="/inventory">Inventory</Link>
-            <Link to="/about">About</Link></div>
-        </nav>
-    );
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <Link to="/shop">Shop</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/inventory">Inventory</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
