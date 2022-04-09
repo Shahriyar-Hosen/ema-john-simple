d64a78be80f778ad6822548615c88639f1698f4a
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-10 02:40:42 +0600

    handler add

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,31 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailValue = (event) => {
+    console.log(event.target.value);
+    setEmail(event.target.value);
+  };
+  const handlePasswordValue = (event) => {
+    console.log(event.target.value);
+    setPassword(event.target.value);
+  };
+  const handleConfirmPasswordValue = (event) => {
+    console.log(event.target.value);
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("your two passwords don't match");
+      return;
+    }
+    
+  };
+
   return (
     <div className="form-container">
       <div>
         <h4 className="form-title">Sign Up</h4>
-        <form>
+        <form onClick={handleCreateUser}>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="" required/>
+            <input
+              onBlur={handleEmailValue}
+              type="email"
+              name="email"
+              id=""
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" password="email" id="" required/>
+            <input
+              onBlur={handlePasswordValue}
+              type="password"
+              password="email"
+              id=""
+              required
+            />
           </div>
           <div className="input-group">
             <label htmlFor="confirm-password">Confirm Password</label>
-            <input type="password" password="confirm-password" id="" required/>
+            <input
+              onBlur={handleConfirmPasswordValue}
+              type="password"
+              password="confirm-password"
+              id=""
+              required
+            />
+            <p style={{ color: "red" }}>{error}</p>
+            <div className="form-submit">
+              <input type="submit" value="Sign Up" />
+            </div>
           </div>
         </form>
-        <div className="form-submit">
-          <input type="submit" value="Sign Up" />
-        </div>
+
         <p>
           Already have an account?
-          <Link className="form-link" to="/signup">
+          <Link className="form-link" to="/login">
             Login
           </Link>
         </p>
